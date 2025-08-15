f5b24ed9253b58da6605dc1b86c15286b3faab4c
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-15 17:39:00 -0300

    Fernanda's Exercise 3

---
+++ Exercises&pratices/FernandaExercices/Exercice_3.cpp
@@ -0,0 +1,19 @@
+/*
+#include <iostream>
+
+void area(double heigth, double width) {
+	double area = (heigth * width) / 2;
+	std::cout << "Area = " << area;
+}
+
+int main() {
+	double height, width;
+	std::cout << "Text the height & width of Triangle: \n";
+	std::cout << "Heigth: ";
+	std::cin >> height;
+	std::cout << "width: ";
+	std::cin >> width;
+	area(height, width);
+	return 0;
+}
+*/
