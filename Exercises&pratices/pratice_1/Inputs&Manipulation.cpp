4d51b953aa8e0b90cc231115ab3c8e6bcf8889b3
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-15 14:39:21 -0300

    .

---
+++ Exercises&pratices/pratice_1/Inputs&Manipulation.cpp
@@ -0,0 +1,48 @@
+/*
+#include <iostream>
+#include <string>
+
+int main() {
+	
+	std::string x;
+	std::cout << "Text what u need:\n [PLUS / SUBTRACT / MULTIPLY / DIVIDE]: ";
+	std::cin >> x;
+
+	if (x._Equal("PLUS")) {
+		int n1, n2;
+		std::cout << "Text 1st Number: ";
+		std::cin >> n1;
+		std::cout << "Text 2nd Number: ";
+		std::cin >> n2;
+		std::cout << "The result is: " << (n1 + n2);
+	}
+	else if (x._Equal("SUBTRACT")) {
+		int n1, n2;
+		std::cout << "Text 1st Number: ";
+		std::cin >> n1;
+		std::cout << "Text 2nd Number: ";
+		std::cin >> n2;
+		std::cout << "The result is: " << (n1 - n2);
+	}
+	else if (x._Equal("MULTIPLY")) {
+		int n1, n2;
+		std::cout << "Text 1st Number: ";
+		std::cin >> n1;
+		std::cout << "Text 2nd Number: ";
+		std::cin >> n2;
+		std::cout << "The result is: " << (n1 * n2);
+	}
+	else if (x._Equal("DIVIDE")) {
+		double n1, n2;
+		std::cout << "Text 1st Number: ";
+		std::cin >> n1;
+		std::cout << "Text 2nd Number: ";
+		std::cin >> n2;
+		std::cout << "The result is: " << (n1 / n2);
+	}
+	else {
+		std::cout << "[ERROR] - Invalid input!";
+	}
+
+}
+*/
---
+++ Exercises&pratices/pratice_1/SimpleThings.cpp
@@ -0,0 +1,15 @@
+/* 
+#iludnce <iostream>
+#include <string>
+
+int main() {
+
+	std::cout << "Hello World!\n";
+	std::string str = "C++ is cool";
+	std::cout << str << "\n";
+
+	const int constNumber = 10;
+	std::cout << "Const Numver: " << constNumber << "\n";
+
+}
+*/
