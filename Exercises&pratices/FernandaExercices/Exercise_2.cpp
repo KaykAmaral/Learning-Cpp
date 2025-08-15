cc00f48c0fd5770ad0a1c9baf0ff4675410f4d51
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-15 16:47:38 -0300

    Fernanda's Exercise 2

---
+++ Exercises&pratices/FernandaExercices/Exercise_2.cpp
@@ -0,0 +1,21 @@
+/*
+#include <iostream>
+
+void area(double heigth, double width) {
+	double area = heigth * width;
+	std::cout << "Area = " << area;
+}
+
+int main() {
+	double height, width;
+	std::cout << "Text the height & width of Rectangle: \n";
+	std::cout << "Heigth: ";
+	std::cin >> height;
+	std::cout << "width: ";
+	std::cin >> width;
+	
+	area(height, width);
+	
+	return 0;
+}
+*/
