211f02872ff747d57ad41f55bd21fa804fbe426a
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-16 13:31:45 -0300

    Fernanda's Exercise 9

---
+++ Exercises&pratices/FernandaExercices/Exercise_9.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+#include <list>
+
+int main() {
+	double a, b, c, d;
+	std::list<double> numbers;
+	
+	std::cout << "Plese, text four numbers\n";
+	std::cout << "1st number: ";
+	std::cin >> a;
+	numbers.push_back(a);
+	std::cout << "2nd number: ";
+	std::cin >> b;
+	numbers.push_back(b);
+	std::cout << "3rd number: ";
+	std::cin >> c;
+	numbers.push_back(c);
+	std::cout << "4th number: ";
+	std::cin >> d;
+	numbers.push_back(d);
+
+	numbers.sort();
+
+	std::cout << "Minor number:  " << numbers.front() << "\n";
+	std::cout << "Bigest number: " << numbers.back() << "\n";
+	std::cout << "Avarage:       " << (a + b + c + d) / 4;
+	
+
+	return 0;
+}
+*/
