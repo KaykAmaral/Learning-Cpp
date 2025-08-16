098e83302d7193a20537dd2718597ab63dd82a35
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-15 21:58:10 -0300

    Fernanda's Exercise 4

---
+++ Exercises&pratices/FernandaExercices/Exercise_4.cpp
@@ -0,0 +1,22 @@
+/*
+#include <iostream>
+
+void area(double smaller, double large, double height) {
+	double area = ((smaller + large) * height) / 2;
+	std::cout << "Area: " << area;
+}
+
+int main() {
+	double smaller, large, height;
+	std::cout << "Text the trapeze values\n";
+	std::cout << "Smaller Base: ";
+	std::cin >> smaller;
+	std::cout << "Large Base: ";
+	std::cin >> large;
+	std::cout << "Height: ";
+	std::cin >> height;
+
+	area(smaller, large, height);
+
+}
+*/
