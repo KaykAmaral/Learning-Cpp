173f224ec45c29f8238b2cd348523aaba217e2d8
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-16 12:05:05 -0300

    Fernanda's Exercise 8

---
+++ Exercises&pratices/FernandaExercices/Exercise_8.cpp
@@ -0,0 +1,34 @@
+/*
+#include <iostream>
+
+void testTriangle(double x, double y, double z) {
+
+	if (x == y || y == z || x == z) {
+		if (x == y && y == z) { // equilateral
+			std::cout << "This triangle is equilateral";
+		}
+		if (x != y || x != z || y != z) { // isosceles
+			std::cout << "This triangle is isosceles";
+		}
+	}
+	if (x != y && x != z && y != z) { // scalene
+		std::cout << "This triangle is scalene";
+	}
+
+}
+
+int main() {
+	double x, y, z;
+	std::cout << "Text 3 triangle angles:\n";
+	std::cout << "1st angle: ";
+	std::cin >> x;
+	std::cout << "2nd angle: ";
+	std::cin >> y;
+	std::cout << "3rd angle: ";
+	std::cin >> z;
+
+	testTriangle(x, y, z);
+
+	return 0;
+}
+*/
