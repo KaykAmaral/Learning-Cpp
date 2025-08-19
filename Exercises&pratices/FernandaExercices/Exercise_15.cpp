3c3922804a6978d4c58c61d66fef2e25a067d3a4
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-19 20:29:06 -0300

    Fernanda's Exercise 15

---
+++ Exercises&pratices/FernandaExercices/Exercise_15.cpp
@@ -0,0 +1,27 @@
+/*
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+	
+	list<int> array;
+
+	for (int i = 0; i < 8; i++) {
+		int x;
+		std::cout << "Text the " << (i + 1) << " number: ";
+		cin >> x;
+		array.push_back(x);
+	}
+
+	array.sort();
+
+	cout << "Array ordened: ";
+
+	for (int n : array) {
+		cout << "  " << n;
+	}
+
+}
+*/
