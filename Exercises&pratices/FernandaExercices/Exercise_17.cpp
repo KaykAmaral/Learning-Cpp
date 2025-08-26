cc070df994a76e02f97e27f867467d1bab0d9a01
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-25 21:23:47 -0300

    Fernanda's Exercise 17

---
+++ Exercises&pratices/FernandaExercices/Exercise_17.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int Array[4][4];
+	int x;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+
+			cout << "Enter the " << (i + 1) << "." << (j + 1) << " number: ";
+			cin >> x;
+			Array[i][j] = x;
+
+		}
+	}
+
+	cout << "\nMain Diagonal:   ";
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+
+			if (i == j) { cout << Array[i][j] << "   "; }
+
+		}
+	}
+
+	return 0;
+}
+*/
