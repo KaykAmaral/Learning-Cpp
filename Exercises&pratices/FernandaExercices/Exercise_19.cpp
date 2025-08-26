f81d2a176f513edf7837efd2658c2e023d1b1e4d
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-26 19:26:15 -0300

    Fernanda's Exercise 19

---
+++ Exercises&pratices/FernandaExercices/Exercise_19.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int Array[2][3];
+	int yarrA[3][2];
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			int x;
+			cout << "Enther the " << (i + 1) << "." << (j + 1) << " number: ";
+			cin >> x;
+			Array[i][j] = x;
+			yarrA[j][i] = x;
+		}
+	}
+
+	cout << "\nInversed Array:\n";
+
+	for (int i = 0; i < 3; i++) {
+		cout << "[   ";
+		for (int j = 0; j < 2; j++) {
+			cout << yarrA[i][j] << "   ";
+		}
+		cout << "]\n";
+	}
+
+	return 0;
+}
+*/
