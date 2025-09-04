9444083829e9c5ddd45afd8dba3f501dac871b5c
KaykAmaral <kayaquiurbano@gmail.com>
2025-09-04 17:53:43 -0300

    Fernanda's Exercise 26

---
+++ Exercises&pratices/FernandaExercices/Exercise_26.cpp
@@ -0,0 +1,35 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int Sum(int x, int y) { return x + y; }
+int Min(int x, int y) { return x - y; }
+int Mul(int x, int y) { return x * y; }
+int Div(int x, int y) { return x / y; }
+
+int main() {
+	int x, y, z;
+	cout << "Enter the 1st number: ";
+	cin >> x;
+	cout << "Enter the 2nd number: ";
+	cin >> y;
+
+	cout << "[ + ] (1)\n[ - ](2)\n[ * ](3)\n[ / ](4)\nWhat u like to do?  ";
+	int z;
+	switch (z) {
+	case 1: 
+		Sum(x, y); break;
+	case 2:
+		Min(x, y); break;
+	case 3:
+		Mul(x, y); break;
+	case 4:
+		Div(x, y); break;
+	default:
+		cout << "[ERROR] -> Invalid Imput";
+	}
+
+	return 0;
+}
+*/
