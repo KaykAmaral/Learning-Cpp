037de9c15e9e6bb1c6d91a26f53f81f005764cf0
KaykAmaral <kayaquiurbano@gmail.com>
2025-09-04 16:57:07 -0300

    Fernanda's Exercise 23

---
+++ Exercises&pratices/FernandaExercices/Exercise_23.cpp
@@ -0,0 +1,20 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+float CalculateSumOf2Squares(float x, float y) {
+	return (x * x) + (y * y);
+}
+
+int main() {
+	float x, y;
+
+	cout << "Enter the 1st number: ";
+	cin >> x;
+	cout << "Enter the 2nd number: ";
+	cin >> y;
+
+	cout << "The result is:  " << CalculateSumOf2Squares(x, y);
+}
+*/
