acca2bef4dd46b908ec480faff0b274b5394624a
KaykAmaral <kayaquiurbano@gmail.com>
2025-09-04 17:33:58 -0300

    Fernanda's Exercise 25

---
+++ Exercises&pratices/FernandaExercices/Exercise_25.cpp
@@ -0,0 +1,28 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int LeapYear(int year) {
+	if (year % 4 == 0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int main() {
+	int x;
+	cout << "Enter a year: ";
+	cin >> x;
+	if (LeapYear(x) == 1) {
+		cout << "This is a Leap Year";
+	}
+	else {
+		cout << "This isn't a Leap Year";
+	}
+
+	return 0;
+}
+*/
