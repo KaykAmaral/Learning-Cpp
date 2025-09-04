833c6cd7f209af344293d1c9f4dffd8147f44e39
KaykAmaral <kayaquiurbano@gmail.com>
2025-09-04 16:28:01 -0300

    .

---
+++ Exercises&pratices/FernandaExercices/Exercise_21.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int BiArray[6][6];
+	int Array[6];
+
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			int x;
+			cout << "Enter the " << (i + 1) << "." << (j + 1) << " number: ";
+			cin >> x; BiArray[i][j] = x;
+		}
+	}
+
+	for (int i = 0; i < 6; i++) {
+		if (i != 5) {
+			Array[i] = BiArray[i][i + 1];
+		}
+		else {
+			Array[i] = 0;
+		}
+	}
+
+	for (int i = 0; i < 6; i++) {
+		
+	}
+
+	return 0;
+}
